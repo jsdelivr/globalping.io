6e3e48066f016b1310919fad90af42152b792703
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-11-12 13:35:27 +0100

    chore: migrate tailwind config to main.css

--- app/pages/cli.vue
+++ app/pages/cli.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="relative flex w-full flex-col items-center justify-center gap-4 duration-200 ease-in-out" :class="{'bg-black/90': switchColor}">
-		<h3 class="text-3xl font-bold duration-300 select-none" :class="{'text-blue-50': switchColor, 'text-blue-950': !switchColor}">Welcome to <span class="cursor-pointer bg-gradient-to-r from-green-400 to-blue-500 bg-clip-text text-transparent" @click="switchColor = !switchColor">Nuxt</span> pages!</h3>
+		<h3 class="text-3xl font-bold duration-300 select-none" :class="{'text-blue-50': switchColor, 'text-initial': !switchColor}">Welcome to <span class="cursor-pointer bg-linear-to-r from-green-400 to-blue-500 bg-clip-text text-transparent" @click="switchColor = !switchColor">Nuxt</span> pages!</h3>
 		<a href="/" class="text-blue-500 hover:underline">Back to Ractive.js pages</a>
 		<small class="hidden">This page uses tailwindcss, if you see this message, it is not working properly.</small>
 	</section>
