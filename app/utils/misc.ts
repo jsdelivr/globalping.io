02110b1857288f32d2e7fb7148a5a72d7111f17e
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-11-18 10:12:13 +0100

    fix: make copyToClipboard client-only

--- app/utils/misc.ts
+++ app/utils/misc.ts
@@ -1,3 +1,3 @@
-export const copyToClipboard = (text: string) => navigator.clipboard.writeText(text);
-
 export const isClient = () => typeof window !== 'undefined';
+
+export const copyToClipboard = (text: string) => isClient() ? navigator.clipboard.writeText(text) : Promise.resolve();
