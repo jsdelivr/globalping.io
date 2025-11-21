f5f2a49924437f798aef7dd1bf63d68aac26522f
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-11-21 18:16:51 +0100

    fix: hide the "Copied" tooltip after a delay

--- app/components/CopyButton.vue
+++ app/components/CopyButton.vue
@@ -1,10 +1,16 @@
 <template>
 	<div class="!absolute">
-		<button v-tooltip.top="'Copy to Clipboard'" class="btn-primary size-10" @click="copyCommand" @mouseleave="hideCopyTooltip" @blur="hideCopyTooltip">
+		<button
+			v-tooltip.top="'Copy to Clipboard'"
+			aria-label="Copy to clipboard"
+			class="btn-primary size-10"
+			@click="copyCommand"
+			@mouseleave="hideCopiedTooltip"
+			@blur="hideCopiedTooltip">
 			<img class="mx-auto size-5" src="~/assets/images/icons/copy.svg" alt="">
 		</button>
 		<!-- This needs to match the v-tooltip styles. -->
-		<div v-if="copyTooltip" role="tooltip" class="p-fadein absolute -top-9 left-1/2 -translate-x-1/2 px-0 py-1">
+		<div v-if="copiedTooltip" role="tooltip" class="p-fadein absolute -top-9 left-1/2 -translate-x-1/2 px-0 py-1">
 			<div class="bg-dark-700 rounded-md p-2 text-[12px] leading-none font-semibold break-words whitespace-pre-line text-white">Copied!</div>
 			<div class="border-t-dark-700 absolute bottom-0 left-1/2 ml-[-10px] h-0 w-0 border-x-[10px] border-t-[10px] border-b-0 border-solid border-transparent"/>
 		</div>
@@ -15,14 +21,23 @@
 	import { copyToClipboard } from '~/utils/misc';
 	const { text } = defineProps<{ text: string }>();
 
-	const copyTooltip = ref(false);
+	const copiedTooltip = ref(false);
+	const tooltipHideTimeout = ref<NodeJS.Timeout>();
 
 	const copyCommand = async () => {
 		await copyToClipboard(text);
-		copyTooltip.value = true;
+		copiedTooltip.value = true;
+
+		clearTimeout(tooltipHideTimeout.value);
+
+		tooltipHideTimeout.value = setTimeout(hideCopiedTooltip, 1000);
 	};
 
-	const hideCopyTooltip = () => {
-		copyTooltip.value = false;
+	const hideCopiedTooltip = () => {
+		copiedTooltip.value = false;
 	};
+
+	onBeforeUnmount(() => {
+		clearTimeout(tooltipHideTimeout.value);
+	});
 </script>
