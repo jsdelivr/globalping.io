378d8fed10ea1965520f384e4d05cec75a33ff1d
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-12-02 13:22:33 +0100

    refactor: update AnimatedNumber component logic

--- app/components/AnimatedNumber.vue
+++ app/components/AnimatedNumber.vue
@@ -3,32 +3,40 @@
 </template>
 
 <script setup lang="ts">
-	const { number } = defineProps<{ number: number }>();
+	const props = defineProps({
+		number: { type: Number, required: true },
+		duration: { type: Number, default: 1500 },
+	});
 
 	const displayedNumber = ref(0);
-	const timeout = ref();
+	const animationReqId = ref();
 
-	const animateChange = (step = 1) => {
-		clearTimeout(timeout.value);
-
-		if (number === displayedNumber.value) {
-			return;
-		}
-
-		timeout.value = setTimeout(() => {
-			if (displayedNumber.value > number) {
-				displayedNumber.value = Math.max(number, displayedNumber.value - step);
-			} else {
-				displayedNumber.value = Math.min(number, displayedNumber.value + step);
-			}
-
-			animateChange(step + 1);
-		}, 10);
+	const easeInOutFn = (val: number) => {
+		const sqr = Math.pow(val, 2);
+		return sqr / (2 * (sqr - val) + 1);
 	};
 
-	watch(() => number, () => {
-		animateChange(1);
+	const animateChange = (init_num: number, start_time: number) => {
+		window?.cancelAnimationFrame(animationReqId.value);
+
+		animationReqId.value = window?.requestAnimationFrame(() => {
+			const curr_duration = Date.now() - start_time;
+			const animation_progress = curr_duration / props.duration;
+
+			if (animation_progress >= 1) {
+				displayedNumber.value = props.number;
+				return;
+			}
+
+			const step = (props.number - init_num) * easeInOutFn(animation_progress);
+			displayedNumber.value = init_num + Math.round(step);
+			animateChange(init_num, start_time);
+		});
+	};
+
+	watch(() => props.number, () => {
+		animateChange(displayedNumber.value, Date.now());
 	}, { immediate: true });
 
-	onUnmounted(() => clearTimeout(timeout.value));
+	onUnmounted(() => window.cancelAnimationFrame(animationReqId.value));
 </script>
