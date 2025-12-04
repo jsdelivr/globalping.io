ce0b01c571332bb933a5f1a378d2c91e3ba29ae5
Pavel Kopecký <pavel.kopecky@icloud.com>
2025-12-04 15:11:45 +0100

    refactor: move custom nuxt prod file handler into a separate file

---
+++ src/lib/capture-node-response/index.js
@@ -0,0 +1,124 @@
+module.exports = async (handler, ctx) => {
+	// let handler process the request, save res body, headers, and status code via a proxy
+	let requestFinished;
+	let processingPromise = new Promise((resolve) => {
+		requestFinished = resolve;
+	});
+
+	let bodyChunks = [];
+	let bodyBuffer = Buffer.alloc(0);
+	let headers = Object.create(null);
+	let capturedStatusCode = 200;
+
+	let resProxy = new Proxy(ctx.res, {
+		get (target, property, receiver) {
+			// do not send anything to the client
+			if ([ 'writeContinue', 'writeEarlyHints', 'flushHeaders' ].includes(property)) {
+				return () => null;
+			}
+
+			if (property === 'setHeader') {
+				return function (name, value) {
+					headers[name.toLowerCase()] = value;
+				};
+			}
+
+			if (property === 'getHeader') {
+				return function (name) {
+					return headers[name.toLowerCase()];
+				};
+			}
+
+			if (property === 'removeHeader') {
+				return function (name) {
+					delete headers[name.toLowerCase()];
+				};
+			}
+
+			if (property === 'writeHead') {
+				return function (statusCode, statusMessage, newHeaders) {
+					capturedStatusCode = statusCode;
+
+					if (typeof statusMessage === 'object' && statusMessage !== null) {
+						newHeaders = statusMessage;
+						statusMessage = null;
+					}
+
+					if (newHeaders) {
+						Object.entries(newHeaders).forEach(([ key, value ]) => {
+							headers[key.toLowerCase()] = value;
+						});
+					}
+
+					return this;
+				};
+			}
+
+			if (property === 'write') {
+				return function (chunk, encoding, callback) {
+					if (typeof encoding === 'function') {
+						callback = encoding;
+						encoding = null;
+					}
+
+					if (typeof chunk === 'string') {
+						chunk = Buffer.from(chunk, encoding ?? 'utf8');
+					}
+
+					bodyChunks.push(chunk);
+					callback?.();
+
+					return true;
+				};
+			}
+
+			if (property === 'end') {
+				return function (chunk, encoding, callback) {
+					if (typeof encoding === 'function') {
+						callback = encoding;
+						encoding = null;
+					}
+
+					if (chunk) {
+						if (typeof chunk === 'string') {
+							chunk = Buffer.from(chunk, encoding || 'utf8');
+						}
+
+						bodyChunks.push(chunk);
+					}
+
+					bodyBuffer = Buffer.concat(bodyChunks);
+
+					callback?.();
+					requestFinished();
+				};
+			}
+
+			return Reflect.get(target, property, receiver);
+		},
+
+		set (target, property, value, receiver) {
+			if (property === 'statusCode') {
+				capturedStatusCode = value;
+				return true;
+			}
+
+			return Reflect.set(target, property, value, receiver);
+		},
+	});
+
+	// pass req to the handler
+	handler(ctx.req, resProxy);
+	await processingPromise;
+
+	// apply captured handler data
+	ctx.status = capturedStatusCode;
+	ctx.set(headers);
+	ctx.body = bodyBuffer;
+
+	// properties for the remaining middleware
+	if (capturedStatusCode < 400) {
+		ctx.res.allowCaching = true;
+		ctx.maxAge = 5 * 60;
+	}
+};
