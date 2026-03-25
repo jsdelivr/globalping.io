4751f954fe24f46a694c6dc3946a90c50d684647
Pavel Kopecký <pavel.kopecky@icloud.com>
2026-03-25 15:47:14 +0100

    feat: add results count to paginator

--- app/pages/leaderboard.vue
+++ app/pages/leaderboard.vue
@@ -59,15 +59,23 @@
 					<LeaderboardUserList :user-list="userList" :loading="loading"/>
 				</div>
 				<ClientOnly>
-					<pvPaginator
-						v-if="userCount"
-						:first="first"
-						:rows="ITEMS_PER_PAGE"
-						:total-records="userCount"
-						:template="template"
-						:page-link-size="pageLinkSize"
-						@page="page = $event.page"
-					/>
+					<div class="relative flex justify-center">
+						<span
+							v-if="userCount > ITEMS_PER_PAGE"
+							class="absolute top-1/2 left-8 -translate-y-1/2 text-xs max-lg:hidden"
+						>
+							Results <strong class="font-medium">{{ first + 1 }} - {{ first + userList.length }}</strong> of <strong class="font-medium">{{ userCount }}</strong>
+						</span>
+						<pvPaginator
+							v-if="userCount"
+							:first="first"
+							:rows="ITEMS_PER_PAGE"
+							:total-records="userCount"
+							:template="template"
+							:page-link-size="pageLinkSize"
+							@page="page = $event.page"
+						/>
+					</div>
 				</ClientOnly>
 			</div>
 		</section>
